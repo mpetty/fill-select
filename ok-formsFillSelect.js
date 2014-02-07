75012193ef32f63c3c1f4453a1e614bfe8c7a1b7
Mitchell Petty <mipett23@gmail.com>
2014-02-07 13:16:25 -0500

    added file for methods

--- ok-formsFillSelect.js
+++ ok-formsFillSelect.js
@@ -2,7 +2,7 @@
  *	Fill Select FIeld
  *
  *	@version 1.0.0
- *	@author Mitchell Petty <mpetty@learninghouse.com>
+ *	@author Mitchell Petty <https://github.com/mpetty/ok-fillSelectField>
  */
 (function($) {
 "use strict";
@@ -74,81 +74,8 @@
 
 		/**
 		 * 	Default methods
-		 *
-		 *	@todo Add more methods
 		 */
-		methods : {
-
-			/** @todo Finish State method */
-			state : function() {
-				var results = [],
-					states = {
-					    "AL": "Alabama",
-					    "AK": "Alaska",
-					    "AS": "American Samoa",
-					    "AZ": "Arizona",
-					    "AR": "Arkansas",
-					    "CA": "California",
-					    "CO": "Colorado",
-					    "CT": "Connecticut",
-					    "DE": "Delaware",
-					    "DC": "District Of Columbia",
-					    "FM": "Federated States Of Micronesia",
-					    "FL": "Florida",
-					    "GA": "Georgia",
-					    "GU": "Guam",
-					    "HI": "Hawaii",
-					    "ID": "Idaho",
-					    "IL": "Illinois",
-					    "IN": "Indiana",
-					    "IA": "Iowa",
-					    "KS": "Kansas",
-					    "KY": "Kentucky",
-					    "LA": "Louisiana",
-					    "ME": "Maine",
-					    "MH": "Marshall Islands",
-					    "MD": "Maryland",
-					    "MA": "Massachusetts",
-					    "MI": "Michigan",
-					    "MN": "Minnesota",
-					    "MS": "Mississippi",
-					    "MO": "Missouri",
-					    "MT": "Montana",
-					    "NE": "Nebraska",
-					    "NV": "Nevada",
-					    "NH": "New Hampshire",
-					    "NJ": "New Jersey",
-					    "NM": "New Mexico",
-					    "NY": "New York",
-					    "NC": "North Carolina",
-					    "ND": "North Dakota",
-					    "MP": "Northern Mariana Islands",
-					    "OH": "Ohio",
-					    "OK": "Oklahoma",
-					    "OR": "Oregon",
-					    "PW": "Palau",
-					    "PA": "Pennsylvania",
-					    "PR": "Puerto Rico",
-					    "RI": "Rhode Island",
-					    "SC": "South Carolina",
-					    "SD": "South Dakota",
-					    "TN": "Tennessee",
-					    "TX": "Texas",
-					    "UT": "Utah",
-					    "VT": "Vermont",
-					    "VI": "Virgin Islands",
-					    "VA": "Virginia",
-					    "WA": "Washington",
-					    "WV": "West Virginia",
-					    "WI": "Wisconsin",
-					    "WY": "Wyoming"
-					};
-
-				results.push(states); // value : name
-				return results;
-			}
-
-		}
+		methods : {}
 
 	};
 
